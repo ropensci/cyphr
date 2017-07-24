15aefc37fdaf63a42d06726ff73479a75aba30f5
boB Rudis <bob@rud.is>
2017-07-24 08:43:35 -0400

    Light spelling fixes and a package-level Rd

---
+++ R/cyphr.R
@@ -0,0 +1,15 @@
+#' High Level Encryption Wrappers
+#'
+#' Encryption wrappers, using low-level support from sodium and openssl.
+#'
+#' It is _strongly_ recommended that you read _both_ vignettes before attempting
+#' to use `cyphr`.
+#'
+#' - [introduction](https://richfitz.github.io/cyphr/vignettes/cyphr.html) (in R: `vignette("cyphr", package = "cyphr")`)'
+#' - [data vignette](https://richfitz.github.io/cyphr/vignettes/data.html) (in R: `vignette("data", package = "cyphr")`)'
+#'
+#' @md
+#' @name cyphr
+#' @docType package
+#' @author Rich FitzJohn (rich.fitzjohn@@gmail.com)
+NULL

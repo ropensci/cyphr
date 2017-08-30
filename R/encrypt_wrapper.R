f8b1970c801b4e1264ff3e5003a5907e69ec6941
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-08-30 06:57:41 +0100

    Add an example

--- R/encrypt_wrapper.R
+++ R/encrypt_wrapper.R
@@ -25,6 +25,35 @@
 ##'
 ##' @param envir Environment in which \code{expr} is to be evaluated.
 ##' @export
+##' @examples
+##' # To do anything we first need a key:
+##' key <- cyphr::key_sodium(sodium::keygen())
+##'
+##' # Encrypted write.csv - note how any number of arguments to
+##' # write.csv will be passed along
+##' path <- tempfile(fileext = ".csv")
+##' cyphr::encrypt(write.csv(iris, path, row.names = FALSE), key)
+##'
+##' # The new file now exists
+##' file.exists(path)
+##'
+##' # ...but it cannot be read with read.csv!
+##' try(read.csv(path, stringsAsFactors = FALSE))
+##'
+##' # Wrap the read.csv call with cyphr::decrypt()
+##' dat <- cyphr::decrypt(read.csv(path, stringsAsFactors = FALSE), key)
+##' head(dat)
+##'
+##' file.remove(path)
+##'
+##' # If you have a function that is not supported you can specify the
+##' # filename argument directly.  For example, with "write.dcf" the
+##' # filename argument is called "file"; we can pass that along
+##' path <- tempfile()
+##' cyphr::encrypt(write.dcf(list(a = 1), path), key, file_arg = "file")
+##'
+##' # Similarly for decryption:
+##' cyphr::decrypt(read.dcf(path), key, file_arg = "file")
 encrypt <- function(expr, key, file_arg = NULL, envir = parent.frame()) {
   encrypt_(substitute(expr), key, file_arg, envir)
 }
